056859ffcf9fc77e71049b3946061e24597ecf9b
variousnabil <varionabilr@gmail.com>
2020-09-11 23:45:51 +0700

    animation text

---
+++ index.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.description', {
+    strings: ['there is nothing here, just a pear-shaped life!', `A software engineer who passionate in solving problems.
+    A javascript enthusiast but ain't close to any possibilities
+    to love others languages * either. Formerly, an android developer (kotlin);
+    Visionary, a full- stack developer (javascript).`],
+    typeSpeed: 70
+})
