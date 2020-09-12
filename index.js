31e403394312db38b275fc32b7d2ae3f029d5bd4
variousnabil <varionabilr@gmail.com>
2020-09-12 11:22:26 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const typed = new Typed('.description', {
-    strings: ['there is nothing here, just a pear-shaped life!', `A software engineer who passionate in solving problems.
+    strings: ['there is nothing here, just a pear-shaped life!', `A software engineer who passionate 
+    in solving problems pragmatically with right tools (not the overkill one) and new-brand tools (if possible).
     A javascript enthusiast but ain't close to any possibilities
-    to love others languages * either. Formerly, an android developer (kotlin);
-    Visionary, a full- stack developer (javascript).`],
+    to love others languages*. Formerly, an android developer (kotlin);
+    Visionary, a full-stack developer (javascript).`],
     typeSpeed: 70
 })
