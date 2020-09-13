62ebd664355bea9bc6c274135c2620929b443bc1
variousnabil <varionabilr@gmail.com>
2020-09-13 20:20:33 +0700

    update minor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const typed = new Typed('.description', {
     strings: ['there is nothing here, just a pear-shaped life!', `A software engineer who passionate 
-    in solving problems pragmatically with right tools (not the overkill one) and new-brand tools (if possible).
+    in solving problems pragmatically with right tools (not the overkill one) and up-to-date tools.
     A javascript enthusiast but ain't close to any possibilities
     to love others languages*. Formerly, an android developer (kotlin);
     Visionary, a full-stack developer (javascript).`],
