62a0fb693cd65be844a93cbf8d16e200693115de
variousnabil <varionabilr@gmail.com>
2021-03-29 13:27:08 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const typed = new Typed('.description', {
-    strings: ['there is nothing here, just a pear-shaped life!', `A Software Engineer from Indonesia who loves to mesmerize the codes to write for itself.`],
+    strings: ['there is nothing here, just a pear-shaped life!', `A Software Engineer from Indonesia who loves to mesmerize the codes to knit for itself. I flourished the horizon with codes. I torn apart the world's calamity with codes. A fidus Achates of Open Source Project. `],
     typeSpeed: 70
 })
